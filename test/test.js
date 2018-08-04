9d8e81f0b827e8048711205266ddbac0ebcde4ac
Eric Downey <edowney29@gmail.com>
2018-08-04 16:46:57 -0400

    Throw Error / updated test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const betterdungeons = require('../better-dungeons');
 
 describe('Dungeon correct', () => {
 	it('should be the correct size', async () => {
-		const dungeon = await betterdungeons(10, 10, 0.3, 3, 3, 'randomseed');
+		const dungeon = await betterdungeons(10, 10, 0.3, 3, 3, new Date().getTime().toString());
 		let counter = 0;
 		for (let row = 0; row < dungeon.gridLength; row++) {
 			for (let column = 0; column < dungeon.gridWidth; column++) {
@@ -12,8 +12,8 @@ describe('Dungeon correct', () => {
 		}
 		assert.equal(counter, 100);
 	});
-	it('should have contains rooms', async () => {
-		const dungeon = await betterdungeons(100, 10, 0.3, 3, 3, 'randomseed');
+	it('should have contain rooms', async () => {
+		const dungeon = await betterdungeons(100, 100, 0.3, 5, 5, new Date().getTime().toString());
 		assert.ok(dungeon.rooms.length > 0);
 	});
 });
