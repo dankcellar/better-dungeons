850299226981d24bdf860fadfa6a54b90bd01e3d
Eric Downey <edowney29@gmail.com>
2019-05-12 18:39:06 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const betterdungeons = require('../better-dungeons');
 
 describe('Create new Dungeon', () => {
   it('should be the correct size', async () => {
-    const dungeon = await betterdungeons(10, 10, 1 / 3, 1, 1, new Date().getTime().toString());
+    const dungeon = await betterdungeons(10, 10, 1 / 3, 2, 2, new Date().getTime().toString());
     let counter = 0;
     for (let row = 0; row < dungeon.gridLength; row += 1) {
       for (let column = 0; column < dungeon.gridWidth; column += 1) {
@@ -18,9 +18,11 @@ describe('Create new Dungeon', () => {
     console.log(`Number of rooms: ${dungeon.rooms.length}`);
     assert.ok(dungeon.rooms.length > 0 && dungeon.paths.length > 0);
   });
+  /*
   it('should set walkable tiles to 0', async () => {
     const dungeon = await betterdungeons(100, 100, 0.3, 5, 5, new Date().getTime().toString());
     console.log(`Walkable tiles: ${dungeon.rooms.length}`);
     assert.ok(dungeon.rooms.length > 0 && dungeon.paths.length > 0);
   });
+  */
 });
